8de3cf9816a99971843fa532e89ca45b0e696278
Paywize <paywize@Paywizes-MacBook-Air.local>
2025-09-25 19:11:47 +0530

    test 2 done

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Git is Working testing"); 
+        System.out.println("Hello, Git is Working and code is push to the Java 2 Repo"); 
     }
 }
